dfdb904270ec9b17f7f8cbe8e3911a7a8a6633fa
y-soliloquy <engineer.soliloquy@gmail.com>
2022-03-26 21:51:14 +0900

    ページにボタンを追加

--- src/views/PiramidMethodView.vue
+++ src/views/PiramidMethodView.vue
@@ -2,6 +2,8 @@
 import UITitle from "../components/ui/UITitle.vue";
 import UIExplain from "../components/ui/UIExplain.vue";
 import UIInput from "../components/ui/UIInput.vue";
+import UIFlexBox from "../components/ui/UIFlexBox.vue";
+import UIButton from "../components/ui/UIButton.vue";
 
 const title = "ピラミッド法";
 const explain =
@@ -12,6 +14,10 @@ const explain =
 	<UITitle :title="title" />
 	<UIExplain :explain="explain" />
 	<UIInput />
+	<UIFlexBox>
+		<UIButton>計算</UIButton>
+		<UIButton>リセット</UIButton>
+	</UIFlexBox>
 </template>
 
 <style></style>
