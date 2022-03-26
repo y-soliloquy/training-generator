7aa8e02591789c1ed8103fbd17c34eee1a1e7c4e
y-soliloquy <engineer.soliloquy@gmail.com>
2022-03-26 20:00:04 +0900

    ページコンポーネントを作成

---
+++ src/views/531MethodView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>531プログラムのページです。</div>
+</template>
+
+<style></style>
---
+++ src/views/PiramidMethodView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>ピラミッド法のページです</div>
+</template>
+
+<style></style>
