ba929efd256c92d6ba48e97950ca8246ff7be0e4
y-soliloquy <engineer.soliloquy@gmail.com>
2022-03-27 12:16:48 +0900

    TOPページにロゴ画像を追加

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,37 @@
 // import { defineComponent } from "vue";
 // import { Ref } from 'vue';
 import HelloVisitors from "@/components/HelloVisitors.vue";
+import UITitle from "../components/ui/UITitle.vue";
 // export default defineComponent({
 //   name: "HomeView",
 //   components: {
 //     HelloWorld,
 //   },
 // });
+const title = "筋トレメニュージェネレーター";
 const text = "筋トレのメニューを自動生成します";
 </script>
 
 <template>
-	<div class="home">
+	<div class="HomeView">
 		<!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+		<UITitle :title="title" />
+		<div class="HomeView-Logo">
+			<img src="../assets/logo_top.png" />
+		</div>
 		<HelloVisitors :text="text" />
 	</div>
 </template>
+
+<style>
+.HomeView-Logo {
+	width: 400px;
+	height: auto;
+	margin: 0px auto;
+}
+
+.HomeView-Logo > img {
+	width: 100%;
+	height: auto;
+}
+</style>
