335ea3049765c78db78ee6b404e0ee7b5a3c2d22
y-soliloquy <engineer.soliloquy@gmail.com>
2022-03-29 21:46:11 +0900

    画像削除

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
 	transpileDependencies: true,
+	pwa: {
+		iconPaths: {
+			favicon32: "favicon.ico",
+		},
+	},
 });
