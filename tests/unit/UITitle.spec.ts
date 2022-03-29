4db195fd09b9c09975abe77945a64e2d5821f530
y-soliloquy <engineer.soliloquy@gmail.com>
2022-03-30 01:57:43 +0900

    UITitleのテストコードを書いてみる

--- tests/unit/UITitle.spec.ts
+++ tests/unit/UITitle.spec.ts
@@ -1,42 +1,62 @@
 import { shallowMount } from "@vue/test-utils";
 import UITitle from "@/components/ui/UITitle.vue";
 
-test("test UITitle Component", () => {
-	const title = "筋トレメニュージェネレーター";
-	const wrapper = shallowMount(UITitle, {
-		props: {
-			title: title,
-		},
-	});
-	expect(wrapper.text()).toMatch(title);
-});
+// test("test UITitle Component", () => {
+// 	const title = "筋トレメニュージェネレーター";
+// 	const wrapper = shallowMount(UITitle, {
+// 		props: {
+// 			title: title,
+// 		},
+// 	});
+// 	expect(wrapper.text()).toMatch(title);
+// });
 
-test("test UITitle Component", () => {
-	const title = "チュートリアル";
-	const wrapper = shallowMount(UITitle, {
-		props: {
-			title: title,
-		},
-	});
-	expect(wrapper.text()).toMatch(title);
-});
+// test("test UITitle Component", () => {
+// 	const title = "チュートリアル";
+// 	const wrapper = shallowMount(UITitle, {
+// 		props: {
+// 			title: title,
+// 		},
+// 	});
+// 	expect(wrapper.text()).toMatch(title);
+// });
 
-test("test UITitle Component", () => {
-	const title = "531プログラム";
-	const wrapper = shallowMount(UITitle, {
-		props: {
-			title: title,
-		},
-	});
-	expect(wrapper.text()).toMatch(title);
-});
+// test("test UITitle Component", () => {
+// 	const title = "531プログラム";
+// 	const wrapper = shallowMount(UITitle, {
+// 		props: {
+// 			title: title,
+// 		},
+// 	});
+// 	expect(wrapper.text()).toMatch(title);
+// });
 
-test("test UITitle Component", () => {
-	const title = "ピラミッド法";
-	const wrapper = shallowMount(UITitle, {
-		props: {
-			title: title,
-		},
+// test("test UITitle Component", () => {
+// 	const title = "ピラミッド法";
+// 	const wrapper = shallowMount(UITitle, {
+// 		props: {
+// 			title: title,
+// 		},
+// 	});
+// 	expect(wrapper.text()).toMatch(title);
+// });
+
+describe("UITitle.vue", () => {
+	it("タイトル文言に使うpropsを渡す", () => {
+		const wrapper = shallowMount(UITitle, {
+			props: {
+				title: "タイトル",
+			},
+		});
+		expect(wrapper.vm.$props.title).toMatch("タイトル");
 	});
-	expect(wrapper.text()).toMatch(title);
+
+	it("スタイル制御に使うpropsを渡す", () => {
+		const wrapper = shallowMount(UITitle, {
+			props: {
+				titleType: "normal",
+			},
+		});
+		expect(wrapper.vm.$props.titleType).toMatch("normal");
+	});
 });
